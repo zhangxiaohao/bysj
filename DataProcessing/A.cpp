9839ac1cf66bded3c61fa535a65fcd350eae9511
zhangxiaohao <915628768@qq.com>
2015-04-19 12:44:13 +0800

    only a test

---
+++ DataProcessing/A.cpp
@@ -0,0 +1,42 @@
+/**************************************************
+ * Author	 : xiaohao Z
+ * Blog	 : http://www.cnblogs.com/shu-xiaohao/
+ * Last modified : 2015-04-19 12:31
+ * Filename	 : A.cpp
+ * Description	 : 
+ * ************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#define MP(a, b) make_pair(a, b)
+#define PB(a) push_back(a)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<unsigned int,unsigned int> puu;
+typedef pair<int, double> pid;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+
+const int INF = 0x3f3f3f3f;
+const double eps = 1E-6;
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+
+	cout << "Hello Word" << endl;
+	return 0;
+}
+
